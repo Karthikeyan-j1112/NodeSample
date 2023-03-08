5a30a19b8701437269923eb2829257cc093eabea
Karthikeyan J <119804748+Karthikeyan-j1112@users.noreply.github.com>
2023-03-08 20:34:43 +0530

    Update PgClient.js

--- PgClient.js
+++ PgClient.js
@@ -1,11 +1,11 @@
 const { Client } = require("pg")
 
 const client = new Client({
-    host :  process.env.DbHOST ||"dpg-cfvqqbvdvk4rro0a7pug-a.singapore-postgres.render.com",
-    user : process.env.DbUSER|| "karthikeyan",
-    port : process.env.DbPORT || 5432,
-    password : process.env.DbPASSWORD || "2X6xh3zXLG5qHtxpaPAcFbYtshMq7gY7",
-    database :  process.env.DB || "musicfolks",
+    host :  process.env.DbHOST ,
+    user : process.env.DbUSER,
+    port : process.env.DbPORT ,
+    password : process.env.DbPASSWORD ,
+    database :  process.env.DB ,
     ssl: true
 });
 

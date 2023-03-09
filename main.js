3fb098ecdf0e5d0a00c287258b1e192c88fcd6d3
Karthikeyan J <119804748+Karthikeyan-j1112@users.noreply.github.com>
2023-03-09 23:32:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ module.exports = (res)=>{
     const client = require('./PgClient')                     
     client.query(`Select * from artist`, async (err,data)=>{                                
         if(!err) {                                                                                             
-            res.writeHead(200,{ "Content-Type": "application/json" })
+            res.writeHead(200,{ "Content-Type": "application/json" , "Access-Control-Allow-Origin": "*" })
             res.end(JSON.stringify(data.rows))                                
         }
         else{                                                            
-            res.writeHead(200,{ "Content-Type": "text/html" })
+            res.writeHead(200,{ "Content-Type": "text/html" , "Access-Control-Allow-Origin": "*" })
             res.end(` <h1> There is an Error!! : ${err} </h1>`)                                  
         }            
     })                   
